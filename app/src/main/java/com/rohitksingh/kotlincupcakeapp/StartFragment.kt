6a6b1313e6683f2ce81d339f78ba7ed7273786e7
Rohit <r4rohit002@gmail.com>
2021-06-20 23:28:59 -0700

    Created Layout

--- app/src/main/java/com/rohitksingh/kotlincupcakeapp/StartFragment.kt
+++ app/src/main/java/com/rohitksingh/kotlincupcakeapp/StartFragment.kt
@@ -33,7 +33,6 @@ class StartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false)
     }
 
